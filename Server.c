390c8b8ed13c607171c89f15af03de7985c7748d
ksoon1985 <ksoon1985@naver.com>
2016-11-15 00:50:54 +0900

    Update Server.c

--- Server.c
+++ Server.c
@@ -8,7 +8,12 @@
 
 int main(int argc, char **argv)
 {
-  WSADATA wsaData;
-  SOCKET listen_sock, connect_sock;
-  SOCKADDR_IN listen_addr, connect_addr;
+	WSADATA wsaData;
+	SOCKET listen_sock, connect_sock;
+	SOCKADDR_IN listen_addr, connect_addr;
+	int sockaddr_in_size;
+	char message[100];
+	if(argc!=2){
+		printf("다음과 같이 사용하세요 : %s <PORT번호>\n", argv[0]);
+		exit(1);
 }
